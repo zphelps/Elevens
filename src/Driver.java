2aa9a179430ac37367a76d1957f582c5063c3e18
Zach Phelps <zachstarphelps@gmail.com>
2021-10-25 14:41:30 -0400

    driver mostly finished

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Scanner s1 = new Scanner(System.in);
+        Board b1 = new Board();
+
+        while(true) {
+            while(b1.cardsLeft() > 0 || b1.areMovesLeft()) {
+                System.out.println(b1.toString());
+
+                String move = "";
+
+                while(true) {
+                    System.out.println("Move? ");
+                    move = s1.nextLine();
+
+                    if(!b1.move(move)) {
+                        System.out.print("Illegal move, try again. ");
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+
+            if(b1.cardsLeft() == 0) {
+                System.out.println("Congratulations, you won!");
+            }
+            else if(!b1.areMovesLeft()) {
+                System.out.println("Sorry, there are no moves left.");
+            }
+
+            System.out.println("Play again? (y/N)");
+            String playAgain = s1.nextLine();
+
+            if(playAgain.equals("N")) {
+                break;
+            }
+        }
     }
 }
