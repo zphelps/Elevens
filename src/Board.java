b73fe83cf703142e573e83a60d824dc2fa88402f
ikminor <iminor@parktudor.org>
2021-10-25 14:39:08 -0400

    Board class: Structuring for move(), implementations for areMovesLeft() and toString()

--- src/Board.java
+++ src/Board.java
@@ -22,7 +22,19 @@ public class Board {
      * @return false if the move was invalid and was not made, true otherwise.
      */
     public boolean move (String input) {
-        return false; // dummy
+
+        // 3-CARD REMOVAL ---------------------
+        if (input.length() == 3) {
+
+        }
+
+        // 2-CARD REMOVAL ---------------------
+        if (input.length() == 2) {
+
+        }
+
+        return false;
+
     }
 
     /**
@@ -34,21 +46,57 @@ public class Board {
     }
 
     /**
-     * UNIMPLEMENTED
      * Assesses whether valid moves are left on the board.
      * @return true if moves are available; false otherwise
      */
     public boolean areMovesLeft() {
-        return false; // dummy
+
+        // Indices for this are off by one from actual values.
+        // Be careful when editing.
+        int[] found = new int[13];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] != null) {
+                    if (board[i][j].value == 10) found[9]++;
+                    else if (board[i][j].value == 11) found[10]++;
+                    else if (board[i][j].value == 12) found[11]++;
+                    else if (board[i][j].value == 13) found[12]++;
+                    else found[board[i][j].value-1]++;
+                }
+            }
+        }
+
+        // Two-pair checks
+        for (int i = 0; i <= 4; i++) {
+            if (found[i] > 0 && found[9-i] > 0) return true;
+        }
+
+        // Three-pair checks
+        if (found[10] > 0 && found[11] > 0 && found[12] > 0) return true;
+
+        return false;
+
     }
 
     /**
-     * UNIMPLEMENTED
      * Returns a string representation of the board.
      * @return string representation of the board
      */
     public String toString() {
-        return "dummy";
+
+        StringBuilder out = new StringBuilder();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                out.append(board[i][j].rank);
+                if (j != 2) out.append(" ");
+            }
+            if (i != 2) out.append("\n");
+        }
+
+        return out.toString();
+
     }
 
     /**
