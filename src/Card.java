6adbdb0ef7ad481fa8029a218c31a90ab7443e22
ikminor <iminor@parktudor.org>
2021-10-25 14:41:54 -0400

    Card class: fixed constructor, comment cleanup

--- src/Card.java
+++ src/Card.java
@@ -10,12 +10,13 @@ public class Card {
         suit = inSuit;
 
 
-        if (2 <= value || 9 <= value) rank = (char)value;
+        if (2 <= value || 9 >= value) rank = (char)value;
         else if (value == 10) rank = 'T';
         else if (value == 11) rank = 'J';
         else if (value == 12) rank = 'Q';
         else if (value == 13) rank = 'K';
-        else rank = 'A';
+        else if (value == 1) rank = 'A';
+        else rank = '?';
     }
 
     public int getValue() {
@@ -30,7 +31,7 @@ public class Card {
         return suit;
     }
 
-    //UNIMPLEMENTED: should return in the form "9H", "KC", etc.
+    // Returns in the form "9H", "KC", etc.
     public String toString() {
         String s = rank + suit + "";
         return s;
