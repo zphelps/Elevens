39de6374bf3487a3bac6bee3975b389f93e15ba5
ikminor <iminor@parktudor.org>
2021-10-26 13:50:31 -0400

    Driver: Removed hello world, fixed win/lose check, removed case-sensitivity on inputs

--- src/Driver.java
+++ src/Driver.java
@@ -1,21 +1,21 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello World");
 
         Scanner s1 = new Scanner(System.in);
         Board b1 = new Board();
 
         while(true) {
-            while(b1.cardsLeft() > 0 || b1.areMovesLeft()) {
+            while(b1.cardsLeft() > 0 && b1.areMovesLeft()) {
                 System.out.println(b1.toString());
 
                 String move = "";
 
                 while(true) {
                     System.out.print("Move? ");
-                    move = s1.nextLine();
+                    move = s1.nextLine().toUpperCase();
 
                     if(!b1.move(move)) {
                         System.out.print("Illegal move, try again. ");
@@ -29,16 +29,19 @@ public class Driver {
             if(b1.cardsLeft() == 0) {
                 System.out.println("Congratulations, you won!");
             }
+
             else if(!b1.areMovesLeft()) {
+                System.out.println(b1.toString());
                 System.out.println("Sorry, there are no moves left.");
             }
 
-            System.out.println("Play again? (y/N)");
-            String playAgain = s1.nextLine();
+            System.out.println("Play again? (Y/N)");
+            String playAgain = s1.nextLine().toUpperCase();
 
-            if(playAgain.equals("N")) {
+            if (playAgain.equals("N")) {
                 break;
             }
         }
+
     }
 }
