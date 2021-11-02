947a3d8e8abd844055c67e484da9663b2b49d630
wmorris05 <wmorris@parktudor.org>
2021-11-02 08:45:04 -0400

    Card class

--- src/Driver.java
+++ src/Driver.java
@@ -6,42 +6,69 @@ public class Driver {
 
         Scanner s1 = new Scanner(System.in);
         Board b1 = new Board();
+        System.out.print("Enter game mode (1=human, 2=computer): ");
+        int gameMode = s1.nextInt();
 
-        while(true) {
-            while(b1.cardsLeft() > 0 && b1.areMovesLeft()) {
-                System.out.println(b1.toString());
+        if (gameMode==1) {
+            while (true) {
+                while (b1.cardsLeft() > 0 && b1.areMovesLeft()) {
+                    System.out.println(b1);
 
-                String move = "";
+                    String move = "";
 
-                while(true) {
-                    System.out.print("Move? ");
-                    move = s1.nextLine().toUpperCase();
+                    while (true) {
+                        System.out.print("Move? ");
+                        move = s1.nextLine().toUpperCase();
 
-                    if(!b1.move(move)) {
-                        System.out.print("Illegal move, try again. ");
-                    }
-                    else {
-                        break;
+                        if (!b1.move(move)) {
+                            System.out.print("Illegal move, try again. ");
+                        } else {
+                            break;
+                        }
                     }
                 }
-            }
 
-            if(b1.cardsLeft() == 0) {
-                System.out.println("Congratulations, you won!");
-            }
+                if (b1.cardsLeft() == 0) {
+                    System.out.println("Congratulations, you won!");
+                } else if (!b1.areMovesLeft()) {
+                    System.out.println(b1);
+                    System.out.println("Sorry, there are no moves left.");
+                }
 
-            else if(!b1.areMovesLeft()) {
-                System.out.println(b1.toString());
-                System.out.println("Sorry, there are no moves left.");
-            }
+                System.out.println("Play again? (y/N)");
+                String playAgain = s1.nextLine().toUpperCase();
 
-            System.out.println("Play again? (y/N)");
-            String playAgain = s1.nextLine().toUpperCase();
-
-            if (playAgain.equals("N")) {
-                break;
+                if (playAgain.equals("N")) {
+                    break;
+                }
+                b1.reset();
             }
-            b1.reset();
+        }
+        else if (gameMode==2){
+            while (true) {
+                while (b1.cardsLeft() > 0 && b1.areMovesLeft()) {
+                    System.out.println(b1);
+
+                    //Computer Moves
+                    //Implement method to get possible move in Board
+
+                }
+
+                if (b1.cardsLeft() == 0) {
+                    System.out.println("The Computer won!");
+                } else if (!b1.areMovesLeft()) {
+                    System.out.println(b1);
+                    System.out.println("Sorry, there are no moves left.");
+                }
+
+                System.out.println("Play again? (y/N)");
+                String playAgain = s1.nextLine().toUpperCase();
+
+                if (playAgain.equals("N")) {
+                    break;
+                }
+                b1.reset();
+            }
         }
 
     }
